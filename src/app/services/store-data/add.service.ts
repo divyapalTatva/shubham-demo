d641e33c39465363c9704f9b301bb2030035ede6
Shubham-Padiya <95012298+Shubham-Padiya@users.noreply.github.com>
2022-12-08 18:38:08 +0530

    environment added

--- src/app/services/store-data/add.service.ts
+++ src/app/services/store-data/add.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +11,16 @@ export class AddService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get(`${environment.apiUrl}`);
   }
 
   addData(info: any) {
-    return this.http.post('https://jsonplaceholder.typicode.com/posts', {
+    return this.http.post(`${environment.apiUrl}`, {
       info
     });
   }
 
   deleteData(id: number) {
-    return this.http.delete('https://jsonplaceholder.typicode.com/posts/' + id);
+    return this.http.delete(`${environment.apiUrl}/${id}`);
   }
 }
